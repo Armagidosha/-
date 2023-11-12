683b949e3705de16b820b787874f4dc7ce239fe9
Armagidoshka <armgidosha@list.ru>
2023-11-12 23:53:27 +0300

    feat: add universal hook for inputs

---
+++ src/utils/hooks.ts
@@ -0,0 +1,15 @@
+import { useState, useCallback } from 'react';
+
+type Value = {
+  [key: string]: any
+}
+
+export const useInput = (event: Value) => {
+  const [inputs, setValues] = useState(event);
+
+  const handleChange = useCallback((event) => {
+    const {value, name} = event.target;
+    setValues({...inputs, [name]: value});
+  }, [inputs]);
+  return {inputs, handleChange};
+}
